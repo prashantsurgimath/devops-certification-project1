b2867f923b12e392db2356ee4c9b724e9e7d4031
shaik-haneef001 <113242964+shaik-haneef001@users.noreply.github.com>
2022-12-12 20:06:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
     description = "Name of security group"
-    default     = "TF-group"
+    default     = "Test-group"
 }
 
 variable "tag_name" {
