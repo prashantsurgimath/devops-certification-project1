15fd609d175af04a84079b9ef8fd5c132d1b28eb
shaik-haneef001 <113242964+shaik-haneef001@users.noreply.github.com>
2022-12-12 18:58:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,13 @@ resource "aws_security_group" "terraform-sg-2022" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
- ingress {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
