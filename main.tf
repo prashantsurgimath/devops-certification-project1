f645bded37d8e7bd8655c8f7277c97837da920ee
shaik-haneef001 <113242964+shaik-haneef001@users.noreply.github.com>
2022-12-13 13:28:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,6 @@
 provider "aws" {
   region = var.aws_region
 }
-resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
-  instance_tenancy = "default"
-  tags = {
-    Name = "main"
-  }
-}
-#Create security group with firewall rules
-resource "aws_security_group" "terraform-sg-2022" {
-  name        = var.security_group
-  description = "security group for Terraform server"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from Jenkins server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
-
 resource "aws_instance" "Test_server" {
   ami           = var.ami_id
   key_name = var.key_name
