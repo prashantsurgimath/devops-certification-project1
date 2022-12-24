c20778fb21a31533afdfe062dea924754bdee9f4
rahulzambre544 <108934359+rahulzambre544@users.noreply.github.com>
2022-12-24 10:59:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
     description = "SSH keys to connect to ec2 instance"
-    default     =  "n.virginia"
+    default     = "N.Virginia"
 }
 
 variable "instance_type" {
