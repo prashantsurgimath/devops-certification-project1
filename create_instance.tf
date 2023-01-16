1563c9cf79616feeae47a03cb007ff8eeef66228
prashantsuragimath <109673973+prashantsuragimath@users.noreply.github.com>
2023-01-16 23:11:26 +0530

    Update create_instance.tf

--- create_instance.tf
+++ create_instance.tf
@@ -1,9 +1,40 @@
-resource "aws_instance" "ap-south-1" {
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAR3EPJ2LQYBJEX535"
+secret_key = "n7UexV6HYnFkI7zjKKwQzSIQQvydVbHu4icQGELJ"
+}
+
+resource "aws_instance" "ec2_example" {
     ami = "ami-0ef82eeba2c7a0eeb"
     instance_type = "t2.micro"
     key_name= "Terraform"
-    security_groups = ["sgr-0153612aaa5d3ff65"]
-  tags= {
-    Name = "TestServer"
- }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
 }
