d8b7c5d7feceb8f4f42bc85226897f53b3d9ea4a
prashantsuragimath <109673973+prashantsuragimath@users.noreply.github.com>
2023-01-16 22:57:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIAR3EPJ2LQ3O2CF27G"
-secret_key = "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"
+access_key = "AKIAR3EPJ2LQ4CRVM7NM"
+secret_key = "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"
 }
 
 resource "aws_instance" "test" {
     ami = "ami-0ef82eeba2c7a0eeb"
     instance_type = "t2.micro"
     key_name= "Terraform"
+    security_groups = ["sgr-0153612aaa5d3ff65"]
   tags= {
     Name = "TestServer"
  }
