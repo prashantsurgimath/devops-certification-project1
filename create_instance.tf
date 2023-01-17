8dc900a651edafe7b2698122a0ca48f60089c7e1
prashantsuragimath <109673973+prashantsuragimath@users.noreply.github.com>
2023-01-17 21:40:35 +0530

    Update create_instance.tf

--- create_instance.tf
+++ create_instance.tf
@@ -7,7 +7,7 @@ secret_key = "n7UexV6HYnFkI7zjKKwQzSIQQvydVbHu4icQGELJ"
 resource "aws_instance" "ec2_example" {
     ami = "ami-0ef82eeba2c7a0eeb"
     instance_type = "t2.micro"
-    key_name= "Terraform"
+    key_name= "Prashant"
 }
 
 resource "aws_security_group" "main" {
